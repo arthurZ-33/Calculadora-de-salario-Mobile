75217add7b2ae082a3147548bd24e96c61a5ea98
arthurZ-33 <arthur.rodrigue64@gmail.com>
2025-10-17 22:22:03 -0300

    v1.0.2

---
+++ app/src/androidTest/java/br/ulbra/calculadoradesalario/Db.java
@@ -0,0 +1,14 @@
+package br.ulbra.calculadoradesalario;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Db extends SQLiteOpenHelper {
+    private static String nome = "db.db";
+    private static int versao=1;
+
+    public Static final TABLE_NAME = "usuarios";
+
+}
