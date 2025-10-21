cf75e74431c8ecc181814c72a18a4f54140ab290
arthurZ-33 <arthur.rodrigue64@gmail.com>
2025-10-20 22:20:30 -0300

    v1.0.9

--- app/src/main/java/br/ulbra/calculadoradesalario/LoginActivity.java
+++ app/src/main/java/br/ulbra/calculadoradesalario/LoginActivity.java
@@ -10,25 +10,26 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class LoginActivity extends AppCompatActivity {
-
     EditText edtxtEmail, edtxtSenha;
-    Button btnLogar;
-    DbHelper dbHelper;
+    Button btnLogar, btnTelaCadastro;
+
+    DbHelper dbHelper; 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Inicializando o DB
-        dbHelper = new DbHelper(this);
 
+        dbHelper = new DbHelper(this);
 
         edtxtEmail = findViewById(R.id.edtxtEmail);
         edtxtSenha = findViewById(R.id.edtxtSenha);
         btnLogar = findViewById(R.id.btnLogar);
+        btnTelaCadastro = findViewById(R.id.btnTelaCadastro);
+
+
 
         btnLogar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,23 +37,38 @@ public class LoginActivity extends AppCompatActivity {
                 String email = edtxtEmail.getText().toString();
                 String senha = edtxtSenha.getText().toString();
 
+                // Validação Rápida
+                if (email.isEmpty() || senha.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Preencha TUDO, não seja um fantasma!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 boolean loginSucesso = dbHelper.checkUser(email, senha);
 
                 if (loginSucesso) {
-                    Toast.makeText(LoginActivity.this, "Acesso liberado! Bem-vindo ao Matrix.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Bem vindo de volta meu patrão", Toast.LENGTH_LONG).show();
 
-                    // Navega para a tela de cálculo APÓS a verificação
+
                     Intent intent = new Intent(LoginActivity.this, CalcularSalarioActivity.class);
                     startActivity(intent);
                     finish();
 
                 } else {
-                    Toast.makeText(LoginActivity.this, "Credenciais inválidas. Tente de novo, Neo.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Acesso negado, tente novamente. O usuário não existe.", Toast.LENGTH_LONG).show();
+
                 }
             }
         });
 
 
+        btnTelaCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 
 
